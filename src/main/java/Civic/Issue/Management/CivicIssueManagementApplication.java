aa2c640959757909660087937b411a91c731afc4
Sherin Joe <sherinjoe0509@gmail.com>
2026-08-08 07:29:25 -0700

    chore: initialize Spring Boot project

---
+++ src/main/java/Civic/Issue/Management/CivicIssueManagementApplication.java
@@ -0,0 +1,13 @@
+package Civic.Issue.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivicIssueManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CivicIssueManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Civic/Issue/Management/CivicIssueManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package Civic.Issue.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivicIssueManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
